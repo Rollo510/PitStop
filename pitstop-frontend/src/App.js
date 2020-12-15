86a3eb5c2953ccda7041208166861f91a836217b
kbritt510 <kbritt510@gmail.com>
2020-12-15 00:53:00 -0600

    Set up initial components

--- pitstop-frontend/src/App.js
+++ pitstop-frontend/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Home from './components/Home'
+import TripsIndex from './components/TripsIndex'
+import TripForm from './components/TripForm'
+import StopsIndex from './components/StopsIndex'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/trips" component={TripsIndex} />
+          <Route path="/trips/new" component={TripForm} />
+          <Route path="/stops" component={StopsIndex} />
+        </Switch>
+      </Router>
     </div>
   );
 }
